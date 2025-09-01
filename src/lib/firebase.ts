457b37269889a34c63c0b44f7306a02b550345a5
Samyam Budhathoki <samyambudhathokisc@gmail.com>
2025-09-01 05:05:00 +0000

    Failed to load resource: net::ERR_BLOCKED_BY_CLIENT ?utm_source=firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   "projectId": "fortress-auth-limmc",
   "appId": "1:927412196214:web:82e4c38a22758704c06057",
   "storageBucket": "fortress-auth-limmc.appspot.com",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCHcx50rmOEv5WjlmoDjHvwHuZcLiMgMfE",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "fortress-auth-limmc.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "927412196214"
